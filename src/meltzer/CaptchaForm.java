d97f189ff933baa915374e4ee77f794d9b29028e
Daniel Meltzer <meltzerdanielebay@gmail.com>
2016-11-14 13:57:58 +0200

    Design captcha form. (By Jonatan Hadas)

---
+++ src/meltzer/CaptchaForm.java
@@ -0,0 +1,12 @@
+package meltzer;
+
+import javax.swing.*;
+
+/**
+ * Created by meltzer on 14/11/2016.
+ */
+public class CaptchaForm {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton newWordButton;
+}
