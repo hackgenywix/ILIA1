4f81d72c15522dc93c4de8900478c8db101f080d
JonatanHadas <jonatan@hadaso.net>
2016-11-15 23:11:45 +0200

    Implement image manipulation.
    image distortion (given an inverse distortion function)
    text adding
    background (initial)
    
    file includes test

---
+++ server_side/captcha_image_distorter.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 22:14:51 2016
+
+@author: Jonatan Hadas
+"""
+
+from PIL import Image,ImageFont,ImageDraw
+import random
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def generate_text(length):
+    '''
+    generates random text of length 'length'
+    '''
+    return ''.join([random.choice(alphabet) for i in xrange(length)])
+    
+def render_text(txt,font_name, font_size,img, pos = (0,0), col = (0,0,0)):
+    '''
+    renders txt on img 
+    '''
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_name, font_size)
+    draw.text(pos,txt,col,font=font)
+    del draw
+    del font
+    
+def empty(size, col = 'white'):
+    '''
+    Creates empty white image.
+    '''
+    return Image.new('L',size,col)
+    
+def back(img, center, c):
+    '''
+    Draw pattern on back
+    '''
+    cx,cy = center
+    w,h = img.size
+    draw = ImageDraw.Draw(img)
+    for x in xrange(w):
+        for y in xrange(h):
+            draw.point((x,y),fill = c*(x-cx)*(y-cy)%256)
+    del draw
+            
+def distort(img,inv_f):
+    '''
+    Distort image img, inverse of distortion 'inv_f' is given.
+    '''
+    w,h = img.size
+    new_img = img.copy()
+    loaded_img = img.load()
+    draw = ImageDraw.Draw(new_img)
+    
+    for x in xrange(w):
+        for y in xrange(h):
+            real_x = float(x)/w
+            real_y = float(y)/h
+            org_x,org_y = inv_f(real_x,real_y)
+            int_x, int_y = int(org_x*w), int(org_y*h)
+            if int_x < 0 or int_y < 0 or int_x > w or int_y > h:
+                col = 255
+            else:
+                col = loaded_img[int_x, int_y]
+            draw.point((x,y),fill = col)
+    
+    del draw
+    return new_img
+    
+#test
+img = empty((200,100))
+back(img,(100,50),3)
+t = generate_text(10)
+render_text(t,'arial.ttf' , 30, img,(10,10),0)
