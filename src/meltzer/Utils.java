c6bcb6c8c5545dfc849909957bc7b3cff07c68b6
Daniel Meltzer <meltzerdanielebay@gmail.com>
2016-11-21 18:41:53 +0200

    Utils

---
+++ src/meltzer/Utils.java
@@ -0,0 +1,40 @@
+package meltzer;
+
+import sun.jvm.hotspot.utilities.BitMap;
+
+/**
+ * Created by meltzer on 21/11/2016.
+ */
+public class Utils {
+    public static void refreshCAPTCHA(){
+        // TODO: change type to Image
+    }
+    public static boolean sendCAPTCHA(String userInput){
+        return true;
+    }
+    public static BitMap browseImage(String path)
+    {
+        return null;
+
+    }
+    public static boolean sendImage(BitMap bitMapImage)
+    {
+        return true;
+    }
+    public static boolean sendCredentials(String username, String password)
+    {
+        return true;
+    }
+    public static boolean changePassword(String password , String newPassword)
+    {
+    return true;
+    }
+    public static boolean updateImage(BitMap newImage)
+    {
+        return true;
+    }
+    public static int[] downloadStats()
+    {
+        return null;
+    }
+}
